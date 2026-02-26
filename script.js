8b7521d2d6104de8d2ace6cf515adba9510b5e08
Yefenny Vargas <yefennyvargas@Mac.lan>
2026-02-25 20:17:56 -0500

    add effect

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 
 function hideEnvelope(){
     const envelope = document.getElementById("front-envelope");
+    const invite = document.getElementById("open-invite");
+    const oEnvelope = document.getElementById("open-envelope");
+    const cEnvelope = document.getElementById("close-envelope");
+    const iLetter = document.getElementById("invite-letter");
 
-    envelope.style.transform = "scale(0)"
+    envelope.style.opacity = "0";
+    envelope.style.display = "none";
+    invite.style.display = "flex";
+    const transitionDuration = 300; // Duration in milliseconds (1 second)
+
+setTimeout(() => {
+   oEnvelope.style.visibility ="visible";
+    oEnvelope.style.opacity = "1"
+    setTimeout(() => {
+       cEnvelope.style.visibility ="visible";
+    cEnvelope.style.opacity = "1"
+        setTimeout(() => {
+       iLetter.style.visibility ="visible";
+    iLetter.style.opacity = "1"
+}, 300);
+}, 300);
+}, transitionDuration);
+
 }
